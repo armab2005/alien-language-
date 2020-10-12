98b5c2be7f79c47652524dce23c0469b02a9cd63
armab2005 <69154178+armab2005@users.noreply.github.com>
2020-10-12 19:26:42 +0530

    Create Amp.py

---
+++ Amp.py
@@ -0,0 +1,27 @@
+from tkinter import *
+import numpy as np
+
+"""
+This section is for fucking tuklu
+"""
+
+#created by Pulastya ® .Inc 
+
+import numpy as np
+am=[382,367,5528,5527,5527,5527,5527,5318,5529,6820,4428,99257,>
+an=[272,26,517,2517,6528,5427,4318,5267,5527,5526,5161,5426,626>
+ar=an+am
+arr=np.array(ar)
+print(arr.shape)
+print ('=========AM STASTICAL MACHINE========')
+arr.reshape((15,2))
+print(arr.reshape((15,2)))
+
+print(arr.reshape((6,5)))
+print(arr.reshape((10,3)))
+print ('======================================')
+
+
+
+print ('======================================')
+print('=========@author:->Arnab Maiti=========')
